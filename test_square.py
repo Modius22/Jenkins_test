249d4487b2e7f596c49f8e6909f936800c1b464a
Christian Piazzi <christian.piazzi@gmx.de>
2020-04-30 16:00:27 +0200

    first commit

---
+++ test_square.py
@@ -0,0 +1,11 @@
+import unittest
+import square as s
+
+
+class SquareTest(unittest.TestCase):
+  def test_square(self):
+   self.assertEqual(s.square(2), 4)
+
+
+if __name__ == '__main__':
+  unittest.main()
