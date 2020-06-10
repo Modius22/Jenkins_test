2aeb21e8d0eeb57ad670447f9f329f8a8e2175e7
Christian Piazzi <christian.piazzi@gmx.de>
2020-06-10 12:45:23 +0200

    set tu true

--- test_square.py
+++ test_square.py
@@ -4,7 +4,7 @@ import square as s
 
 class SquareTest(unittest.TestCase):
   def test_square(self):
-   self.assertEqual(s.square(2), 5)
+   self.assertEqual(s.square(2), 4)
 
 
 if __name__ == '__main__':
